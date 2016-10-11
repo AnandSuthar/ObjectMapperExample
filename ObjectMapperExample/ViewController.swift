234d795558a3e5ccd4c842aa6302d8f07b657b67
anand k <aanand.suthar1987@gmail.com>
2016-10-11 13:08:18 +0530

    Initial commit

---
+++ ObjectMapperExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ObjectMapperExample
+//
+//  Created by mac on 11/10/16.
+//  Copyright © 2016 mac. All rights reserved.
+//
+
+import UIKit
+import ObjectMapper
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'ObjectMapperExample' do
+pod 'ObjectMapper', '~> 2.1'
+end
+
